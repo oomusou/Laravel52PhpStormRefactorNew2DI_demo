cff5778ea38a50a44dd33812967c454d4cc7b550
oomusou <oomusou@gmail.com>
2016-07-08 12:45:35 +0800

    建立 Post 的 ModelFactory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,9 +13,19 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
+        'name'           => $faker->name,
+        'email'          => $faker->safeEmail,
+        'password'       => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
 });
+
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title'       => $faker->name,
+        'description' => $faker->name,
+        'content'     => $faker->text,
+    ];
+});
+
