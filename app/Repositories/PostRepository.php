5fcb2ba66fe0a6f82ca2698b8a7f7925a88d2f8c
oomusou <oomusou@gmail.com>
2016-07-08 12:47:30 +0800

    建立 PostRepository

---
+++ app/Repositories/PostRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Post;
+use Illuminate\Database\Eloquent\Collection;
+
+class PostRepository
+{
+
+    /**
+     * @return Collection
+     */
+    public function getLatest3Posts()
+    {
+        return Post::orderBy('id', 'desc')
+            ->take(3)
+            ->get();
+    }
+}
