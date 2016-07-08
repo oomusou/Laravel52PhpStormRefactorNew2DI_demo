682ab1054e279860ffe7ae31cf3ec61ba7e35dfc
oomusou <oomusou@gmail.com>
2016-07-08 15:40:33 +0800

    將 PostService 重構成依賴注入

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -7,12 +7,23 @@ use Illuminate\Database\Eloquent\Collection;
 
 class PostService
 {
+    /** @var PostRepository */
+    private $postRepository;
+
+    /**
+     * PostService constructor.
+     * @param PostRepository $postRepository
+     */
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
     /**
      * @return Collection
      */
     public function showLatest3Posts()
     {
-        $postRepository = new PostRepository();
-        return $postRepository->getLatest3Posts();
+        return $this->postRepository->getLatest3Posts();
     }
 }
